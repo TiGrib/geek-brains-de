edf38ae216eb66e0b85754911d81cfe190e695a9
Kirill <k.yakimenkov@haut.ai>
2021-10-21 09:43:40 +0300

    lint fixes

--- PythonIntro/yakimenkov_1-2.py
+++ PythonIntro/yakimenkov_1-2.py
@@ -1,14 +1,13 @@
-
-odd_cube = [x**3 for x in range(1, 101) if x%2 == 1]
+odd_cube = [x ** 3 for x in range(1, 101) if x % 2 == 1]
 
 sum_of_7 = 0
 for number in odd_cube:
-    
+
     sum_of_digs = 0
-    while(number != 0):
-        sum_of_digs += (number % 10)
+    while number != 0:
+        sum_of_digs += number % 10
         number = number // 10
-      
+
     if sum_of_digs % 7 == 0:
         sum_of_7 += sum_of_digs
 
@@ -18,12 +17,12 @@ print(sum_of_7)
 
 sum_of_7_plus_17 = 0
 for number in odd_cube:
-    number += 17    
+    number += 17
     sum_of_digs = 0
-    while(number != 0):
-        sum_of_digs += (number % 10)
+    while number != 0:
+        sum_of_digs += number % 10
         number = number // 10
-      
+
     if sum_of_digs % 7 == 0:
         sum_of_7_plus_17 += sum_of_digs
 
--- PythonIntro/yakimenkov_1-3.py
+++ PythonIntro/yakimenkov_1-3.py
@@ -1,4 +1,4 @@
-for number in range(1, 101):    
+for number in range(1, 101):
     if number % 10 == 1 and (number > 20 or number < 10):
         print(f"{number} процент")
     elif 2 <= number % 10 <= 4 and (number > 20 or number < 10):
