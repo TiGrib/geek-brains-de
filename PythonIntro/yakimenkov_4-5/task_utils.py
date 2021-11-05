0b6cc09209d40ab55d6190d1300e38e31589b1d5
Kirill <k.yakimenkov@haut.ai>
2021-11-05 19:00:06 +0300

    add task 5(4)

---
+++ PythonIntro/yakimenkov_4-5/task_utils.py
@@ -0,0 +1,21 @@
+import requests
+import xml.etree.ElementTree as ET
+
+from datetime import datetime
+
+
+def currency_rates(cur_code: str):
+    response  = requests.get(
+        url="http://www.cbr.ru/scripts/XML_daily.asp"
+    )
+    date_str = "-".join(response.headers["Date"].split()[1:4])
+    date = datetime.strptime(date_str, "%d-%b-%Y").date()
+    xml_response = ET.fromstring(response.text)
+
+    for currency in xml_response:
+        if currency.find("CharCode").text == cur_code.upper():
+            return currency.find("Value").text, date
+
+
+if __name__ == "__main__":
+    currency_rates("usd")
