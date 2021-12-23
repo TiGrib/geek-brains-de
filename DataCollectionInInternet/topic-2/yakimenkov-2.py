b54922986d716d83d72d98052d1e5b5af299a30b
Якименков Кирилл Александрович <kayakimenk@www.mts.ru>
2021-12-23 19:41:10 +0300

    RAW scrapper

--- DataCollectionInInternet/topic-2/yakimenkov-2.py
+++ DataCollectionInInternet/topic-2/yakimenkov-2.py
@@ -1,3 +1,4 @@
+from typing import Dict
 import requests
 
 from pprint import pprint
@@ -11,45 +12,71 @@ def hh_get_vacancies(name: str, page_num: int):
         "text": name,
         "page": page_num,
     }
-    headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"
+    }
     response = requests.get(main_url + "search/vacancy", params=params, headers=headers)
-    return bs(response.text, 'html.parser')
+    return bs(response.text, "html.parser")
+
+
+def unicode_to_int(salary: str) -> int:
+    return int("".join([i for i in splited_salary[0] if i != u"\u202f"]))
+
+
+def vacancy(name, url, min_salary=None, max_salary=None, currency=None) -> Dict:
+    return {
+        "name": name,
+        "url": url,
+        "min_salary": min_salary,
+        "max_salary": max_salary,
+        "currency": currency,
+    }
 
 
 if __name__ == "__main__":
     name = "python"
-    pages = 2
+    pages = 5
     vac_list = []
-    for page_num in range(1, pages+1):
-        print(page_num)
-        vacancies = hh_get_vacancies(name, page_num).find_all("div", {"class": "vacancy-serp-item"})
+    for page_num in range(1, pages + 1):
+        vacancies = hh_get_vacancies(name, page_num).find_all(
+            "div", {"class": "vacancy-serp-item"}
+        )
         for vac in vacancies:
             info = vac.find("a", {"data-qa": "vacancy-serp__vacancy-title"})
-            # print(info.text)
-            # print(info.get("href").split("?")[0])
             try:
-                salary = vac.find("span", {"data-qa": "vacancy-serp__vacancy-compensation"})
+                salary = vac.find(
+                    "span", {"data-qa": "vacancy-serp__vacancy-compensation"}
+                )
                 splited_salary = salary.text.split(" ")
                 if splited_salary[1] == "–":
-                    print(splited_salary)
-                    print(info.text)
-                    print(info.get("href").split("?")[0])
-                    print("="*120)
-                    print(splited_salary[0])
-                    print(int(splited_salary[2]))
-                    # print(splited_salary[3])
-                    # print(splited_salary)
-                    # print(
-                    #     {"name": info.text,
-                    #      "url": info.get("href").split("?")[0],
-                    #      "min_salary": int(splited_salary[0]),
-                    #      "max_salary": int(splited_salary[2]),
-                    #      "currency": splited_salary[3]}
-                    #     )
-                # elif splited_salary[0] == "от":
-                #     print(salary.text)
-                # elif splited_salary[0] == "до":
-                #     print(salary.text)
+                    vac_list.append(
+                        vacancy(
+                            info.text,
+                            info.get("href").split("?")[0],
+                            unicode_to_int(splited_salary[0]),
+                            unicode_to_int(splited_salary[2]),
+                            splited_salary[3],
+                        )
+                    )
+                elif splited_salary[0] == "от":
+                    vac_list.append(
+                        vacancy(
+                            info.text,
+                            info.get("href").split("?")[0],
+                            min_salary=unicode_to_int(splited_salary[1]),
+                            currency=splited_salary[2],
+                        )
+                    )
 
+                elif splited_salary[0] == "до":
+                    vac_list.append(
+                        vacancy(
+                            info.text,
+                            info.get("href").split("?")[0],
+                            max_salary=unicode_to_int(splited_salary[1]),
+                            currency=splited_salary[2],
+                        )
+                    )
             except:
                 continue
+    pprint(vac_list)
