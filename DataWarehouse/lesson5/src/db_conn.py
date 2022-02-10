7fa9c702732f7ca2d0e5908b56e58c3ad9d4682c
Якименков Кирилл Александрович <kayakimenk@mts.ru>
2022-02-10 21:23:29 +0300

    add main script

--- DataWarehouse/lesson5/src/db_conn.py
+++ DataWarehouse/lesson5/src/db_conn.py
@@ -15,4 +15,13 @@ class DBConnector:
         conn_string = f"host={self.host} port={self.port} dbname={self.db_name} user={self.user} password={self.password}"
         return psycopg2.connect(conn_string).cursor()
 
-    def 
+    def save_table_to_csv_file(self, table_name):
+        query = f"COPY {table_name} TO STDOUT WITH DELIMITER ',' CSV HEADER"
+        with open(f"../data/{table_name}.csv", "w") as stream:
+            self.cursor.copy_expert(query, stream)
+        print(f"{table_name} successfully saved")
+
+    def insert_to_table_from_csv(self, table_name, streamed_file):
+        query = f"COPY {table_name} from STDIN WITH DELIMITER ',' CSV HEADER"
+        self.cursor.copy_expert(query, streamed_file)
+        print(f"insert data from csv to {table_name}")
